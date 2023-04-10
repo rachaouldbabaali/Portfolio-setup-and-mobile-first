337a8e48c7fec75167448e8585d736f1a9c4e3fd
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-04-10 14:16:49 +0100

    Creates JS file and variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const menuOpen = document.querySelector('.bi-list');
+const containerNav = document.querySelector('.container-nav');
+const menuClose = document.querySelector('.close-hamb');
+const navList = document.querySelector('.nav-list');
+const about = document.querySelectorAll('.mobile-nav-links');
