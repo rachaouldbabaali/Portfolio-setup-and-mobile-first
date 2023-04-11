2c51b6db88d1c40eec4f0763f8fe857183f8f7b0
racha ouldbabaali <rachaould@gmail.com>
2023-04-11 05:49:47 +0100

    Add mobile menu

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const menuOpen = document.querySelector('.bi-list');
-const containerNav = document.querySelector('.container-nav');
+const navBarItems = document.querySelector('.nav-bar').children;
 const menuClose = document.querySelector('.close-hamb');
-const navList = document.querySelector('.nav-list');
 const about = document.querySelectorAll('.mobile-nav-links');
+// array distructuring 
+const [navBarName, navList, menuOpen, containerNav] = navBarItems
+// console.log('object', navBarName, navBarLink, navBarHumb, navBarContainer);
+
+menuOpen.addEventListener('click', () => {
+  containerNav.style.display = 'block'
+  navList.style.display = 'none'
+  menuOpen.style.display = 'none';
+  navBarName.style.display = 'none'
+});
+
+menuClose.addEventListener('click', () => {
+  containerNav.style.display = 'none'
+  menuOpen.style.display = 'flex';
+  navBarName.style.display = 'flex'
+});
+
