0acc8ca2c1adfff1f70b43915d1991b14d9268da
racha ouldbabaali <rachaould@gmail.com>
2023-04-11 05:59:52 +0100

    Add eslint

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const navBarItems = document.querySelector('.nav-bar').children;
 const menuClose = document.querySelector('.close-hamb');
-const about = document.querySelectorAll('.mobile-nav-links');
-// array distructuring 
-const [navBarName, navList, menuOpen, containerNav] = navBarItems
+// array distructuring
+const [navBarName, navList, menuOpen, containerNav] = navBarItems;
 // console.log('object', navBarName, navBarLink, navBarHumb, navBarContainer);
 
 menuOpen.addEventListener('click', () => {
-  containerNav.style.display = 'block'
-  navList.style.display = 'none'
+  containerNav.style.display = 'block';
+  navList.style.display = 'none';
   menuOpen.style.display = 'none';
-  navBarName.style.display = 'none'
+  navBarName.style.display = 'none';
 });
 
 menuClose.addEventListener('click', () => {
-  containerNav.style.display = 'none'
+  containerNav.style.display = 'none';
   menuOpen.style.display = 'flex';
-  navBarName.style.display = 'flex'
+  navBarName.style.display = 'flex';
 });
-
