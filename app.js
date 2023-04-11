6c08a6ba4e9f7dfb8ad1b1ee316087bba6c124a9
racha ouldbabaali <rachaould@gmail.com>
2023-04-11 08:57:36 +0100

    Fix mobile menu disappears when the user clicks any of its options.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const navBarItems = document.querySelector('.nav-bar').children;
 const menuClose = document.querySelector('.close-hamb');
+const about = document.querySelectorAll('.mobile-nav-links');
 // array distructuring
 const [navBarName, navList, menuOpen, containerNav] = navBarItems;
 // console.log('object', navBarName, navBarLink, navBarHumb, navBarContainer);
@@ -15,4 +16,14 @@ menuClose.addEventListener('click', () => {
   containerNav.style.display = 'none';
   menuOpen.style.display = 'flex';
   navBarName.style.display = 'flex';
+  navList.style.display = 'block';
+});
+
+about.forEach((n) => {
+  n.addEventListener('click', () => {
+    containerNav.style.display = 'none';
+    menuOpen.style.display = 'flex';
+    navBarName.style.display = 'flex';
+    navList.style.display = 'block';
+  });
 });
