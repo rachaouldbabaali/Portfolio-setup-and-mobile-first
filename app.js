dea4076d5dc4c10a65191cfecff8da00b25c753d
racha ouldbabaali <rachaould@gmail.com>
2023-04-13 01:14:34 +0100

    Add function that create popups dinamicaly

--- app.js
+++ app.js
@@ -75,9 +75,12 @@ const projects = [
   },
 ];
 
-projects.forEach((element, i) => {
+const projectContainer = document.querySelector('#projects');
+
+// eslint-disable-next-line array-callback-return
+projects.map((element, i) => {
   // create all elements used for card
-  const projectContainer = document.querySelector('#projects');
+
   const projectDiv = document.createElement('div');
   const projectImageDiv = document.createElement('div');
   const projectContentDiv = document.createElement('div');
@@ -147,7 +150,7 @@ projects.forEach((element, i) => {
 
   projectContainer.appendChild(projectDiv);
 
-  projectBtn.innerHTML = '<button data-modal-target="#modal" class="btn project-link" >See project</button>';
+  projectBtn.innerHTML = `<button id="${i}" data-modal-target="#modal" class="btn project-link" >See project</button>`;
   projectDiv.classList.add('card-reverse');
 
   // reverse  card
@@ -160,102 +163,95 @@ projects.forEach((element, i) => {
 
 // Menu PopUP
 const modalContainer = document.querySelector('.popup');
-const modal = document.createElement('div');
-modalContainer.append(modal);
-modal.innerHTML = `<div class="modal" id="modal">
-<div class="modal-header">
-  <h4 class="modal-heading">Tonic</h4>
-  <h4 data-close-button class="modal-close">&times;</h4>
-</div>
-<div class="small-description">
-<p class="canopy modal-describe">CANOPY</p>
-<img
-  src="./images/Counter.svg"
-  class="count-vector"
-  alt="null"
-/>
-<p class="modal-describe">BACK END DEV</p>
-<img
-  src="./images/Counter.svg"
-  class="count-vector"
-  alt="null"
-/>
-<p class="modal-describe">2015</p>
-</div>
-<img
-src="./images/1st-project-image.png"
-class="modal-img"
-alt=""
-/>
-<div class="modal-content">
-<p class="modal-texts">
-  lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis voluptatibus error veritatis
-  ad minima nesciunt harum sed, fugiat eum quis, architecto impedit inventore nam. Assumenda repellendus
-  <br>
-  iste cum consequatur, unde porro laboriosam velit. Consectetur repellendus
-  illo iure reiciendis repudiandae voluptatum velit, placeat a culpa ut nemo aut suscipit? Pariatur, a.
-</p>
-<div class="modal-contact">
-  <ul class="languages modal-ul">
-    <li class="language-used modal-li">HTML</li>
-    <li class="language-used modal-li">CSS</li>
-    <li class="language-used modal-li">Bootstrap</li>
-    <li class="language-used modal-li">gitHub</li>
-    <li class="language-used modal-li">Ruby</li>
-    <li class="language-used modal-li">javaScript</li>
-  </ul>
-  <hr class="modal-hr" />
-  <a href="https://rachaouldbabaali.github.io/Portfolio-setup-and-mobile-first/index.html">
-    <button class="btn modal-btn" type="submit">
-      See live
-      <img
-        src="./images/git-modalicon.svg"
-        class="modal-vectors"
-        alt="git"
-      />
-    </button>
-  </a>
-  <a href="https://github.com/rachaouldbabaali/Portfolio-setup-and-mobile-first">
-    <button class="btn modal-btn" type="submit">
-      See Source
-      <img
-        src="./images/source-code-icon.svg"
-        class="modal-vectors"
-        alt="source-code"
-      />
-    </button>
-  </a>
-</div>
-</div>
-
-`;
-
 const openModalButton = document.querySelectorAll('[data-modal-target]');
-const closeModalButton = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
-function openModal(modal) {
-  if (modal == null) return;
-  modal.classList.add('active');
+function creatPopup(imageSource) {
+  const modal = document.createElement('div');
+  modal.innerHTML = `<div class="modal active" id="modal">
+  <div class="modal-header">
+    <h4 class="modal-heading">Tonic</h4>
+    <h4 data-close-button onclick="closeModal()"class="modal-close">&times;</h4>
+  </div>
+  <div class="small-description">
+  <p class="canopy modal-describe">CANOPY</p>
+  <img
+    src="./images/Counter.svg"
+    class="count-vector"
+    alt="null"
+  />
+  <p class="modal-describe">BACK END DEV</p>
+  <img
+    src="./images/Counter.svg"
+    class="count-vector"
+    alt="null"
+  />
+  <p class="modal-describe">2015</p>
+  </div>
+  <img
+  src="${imageSource}"
+  class="modal-img"
+  alt=""
+  />
+  <div class="modal-content">
+  <p class="modal-texts">
+    lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis voluptatibus error veritatis
+    ad minima nesciunt harum sed, fugiat eum quis, architecto impedit inventore nam. Assumenda repellendus
+    <br>
+    iste cum consequatur, unde porro laboriosam velit. Consectetur repellendus
+    illo iure reiciendis repudiandae voluptatum velit, placeat a culpa ut nemo aut suscipit? Pariatur, a.
+  </p>
+  <div class="modal-contact">
+    <ul class="languages modal-ul">
+      <li class="language-used modal-li">HTML</li>
+      <li class="language-used modal-li">CSS</li>
+      <li class="language-used modal-li">Bootstrap</li>
+      <li class="language-used modal-li">gitHub</li>
+      <li class="language-used modal-li">Ruby</li>
+      <li class="language-used modal-li">javaScript</li>
+    </ul>
+    <hr class="modal-hr" />
+    <a href="https://rachaouldbabaali.github.io/Portfolio-setup-and-mobile-first/index.html">
+      <button class="btn modal-btn" type="submit">
+        See live
+        <img
+          src="./images/git-modalicon.svg"
+          class="modal-vectors"
+          alt="git"
+        />
+      </button>
+    </a>
+    <a href="https://github.com/rachaouldbabaali/Portfolio-setup-and-mobile-first">
+      <button class="btn modal-btn" type="submit">
+        See Source
+        <img
+          src="./images/source-code-icon.svg"
+          class="modal-vectors"
+          alt="source-code"
+        />
+      </button>
+    </a>
+  </div>
+  </div>
+  `;
+  modalContainer.append(modal);
+}
+
+function openModal(imgSource) {
+  creatPopup(imgSource);
   overlay.classList.add('active');
 }
 
-function closeModal(modal) {
-  if (modal == null) return;
-  modal.classList.remove('active');
+// eslint-disable-next-line no-unused-vars
+function closeModal() {
+  const modal = modalContainer.children[0];
+  modalContainer.removeChild(modal);
   overlay.classList.remove('active');
 }
 
 openModalButton.forEach((button) => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget);
-    openModal(modal);
-  });
-});
-closeModalButton.forEach((button) => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal');
-    closeModal(modal);
+  button.addEventListener('click', (event) => {
+    openModal(projects[event.target.id].featuredImg);
   });
 });
