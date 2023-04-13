249e5449079b8e3574302f1f3643c543387e76cf
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-04-13 09:10:30 +0100

    add descriptios

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ function creatPopup(imageSource) {
     lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis voluptatibus error veritatis
     ad minima nesciunt harum sed, fugiat eum quis, architecto impedit inventore nam. Assumenda repellendus
-    <br>
+    <br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
     iste cum consequatur, unde porro laboriosam velit. Consectetur repellendus
     illo iure reiciendis repudiandae voluptatum velit, placeat a culpa ut nemo aut suscipit? Pariatur, a.
   </p>
